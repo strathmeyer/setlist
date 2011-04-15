103c3df3d0b08e94db51b60dd842a4785b180808
Eric <eric@vawks.com>
2011-04-14 19:15:45 -0500

    Actually started the app

--- app.rb
+++ app.rb
@@ -3,25 +3,45 @@ require 'sinatra/base'
 require 'redis'
 require 'slim'
 require 'less'
+require 'digest/sha1'
 
 
 class MyApp < Sinatra::Base
+	set :redis, 'redis://localhost:6379/0'
+	enable :sessions
+	set :session_secret, '34nnt0b09isn3j23nrkj3rn23jndj90b90j0dfi4moimoinbgnbklmo'
+	enable :method_override
+
 	redis = Redis.new
 
-	set :redis, 'redis://localhost:6379/0'
-	disable :logging
 
-	redis.set "song", 320
+	before do
+		redis.mset 'user/1/email', 'eric@vawks.com',
+			'user/1/password', myhash('rules')
+		
+		# is the user logged in?
+		if true
+			id = 1
 
-	get '/:x?' do
-		@name = nice_time(redis.get 'song')
-		puts params[:x].inspect
-		if params[:x] == 'x'
-			@name = 'grrrr'
-			slim :foo, :layout => :xlayout	
+			# yes? set the @user variable
+			@user = {}
+			@user[:id] = id
+			@user[:email] = redis.get 'user/' << id << '/email'	
+			@user[:bands] = redis.get 'user/' << id << '/bands'	
 		else
-			slim :foo
+			# no? redirect to login
+			throw "barf"
 		end
+	end
+
+
+	get '/' do
+		@bands = @user[:bands]
+		slim :bands
+	end
+
+	get '/dashboard' do
+		slim :dashboard
 	end
 end
 
@@ -36,4 +56,9 @@ def nice_time(minutes)
 	out << "#{min}m" if min > 0
 
 	out.join(' ')	
+end
+
+def myhash(input)
+	Digest::SHA1.hexdigest('zx0-cv8zxco90.,32m4n2.,3m4noadf' << \
+			Digest::SHA1.hexdigest(input.to_s << 'woweifnweofinweofi'))
 end
